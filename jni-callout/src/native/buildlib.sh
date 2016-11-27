ffa7af2b5cfccd09bbe7512eca9cb54409552181
Junhan Hu <junhanh@gmail.com>
2016-11-27 09:37:20 -0600

    Microbenchmark for jni callout
    Microbenchmark for jni callout

---
+++ jni-callout/src/native/buildlib.sh
@@ -0,0 +1,1 @@
+gcc -o libjnicallout.so -lc -shared -I$JAVA_HOME/include -I$JAVA_HOME/include/linux jnicallout.c
---
+++ jni-callout/src/native/jnicallout.c
@@ -0,0 +1,7 @@
+#include <jni.h>
+#include <stdio.h>
+#include "com_advos2016_MyBenchmark.h"
+
+JNIEXPORT void JNICALL Java_com_advos2016_MyBenchmark_callout(JNIEnv *env, jobject obj){
+	return;
+}
